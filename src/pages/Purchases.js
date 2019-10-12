8e70e35c232b001cad3314fa1e09440f91152d4b
Brock Duncan <bduncan@tkg.com>
2019-10-12 13:23:37 -0400

    adds animated button

--- src/pages/Purchases.js
+++ src/pages/Purchases.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Header } from 'semantic-ui-react'
-import { Form, Button } from 'semantic-ui-react'
+import { Form, Button, Icon } from 'semantic-ui-react'
 
 export default function Purchases() {
   return (
     <>
       <Header>Purchases</Header>
       <p>Fill out this form to submit a purchase request.</p>
-      <Form>
+      <Form style={{ marginTop: 20 }}>
         <Form.Group>
           <Form.Input label="Your Name" type="text" width={6} />
           <Form.Input label="Email Address" type="email" width={6} />
@@ -48,7 +48,12 @@ export default function Purchases() {
           />
           <Form.Input label="$ Purchase Price" type="number" width={3} />
         </Form.Group>
-        <Button type="submit">Submit</Button>
+        <Button primary animated="fade" type="submit" style={{ marginTop: 20 }}>
+          <Button.Content visible>Submit</Button.Content>
+          <Button.Content hidden>
+            <Icon name="paper plane" />
+          </Button.Content>
+        </Button>
       </Form>
     </>
   )
